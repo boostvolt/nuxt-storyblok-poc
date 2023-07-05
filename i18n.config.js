8162b748cfd78d231c220d51143eda38cfb9ad8f
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2023-07-05 22:14:20 +0200

    initial commit

---
+++ i18n.config.js
@@ -0,0 +1,12 @@
+export default defineI18nConfig(() => ({
+  legacy: false,
+  locale: "de-ch",
+  messages: {
+    "de-ch": {
+      welcome: "Welcome",
+    },
+    "fr-ch": {
+      welcome: "Alooha",
+    },
+  },
+}));
